2774097073635d4672ff6bcd29407a7782871b85
baptiste1908 <baptiste.gilleron@student.junia.com>
2023-11-15 17:39:22 +0100

    the beginning

--- Tram-project/Tram-project.cpp
+++ Tram-project/Tram-project.cpp
@@ -5,8 +5,20 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+
+int main() {
+    // Exemple d'utilisation des classes
+    Rame rame1(1, 0.0, 30.0, 50);
+    Rame rame2(2, 10.0, 25.0, 40);
+
+    Station station1;
+    station1.nombrePassagersQuai = 15;
+
+    Superviseur superviseur;
+    superviseur.rames.push_back(rame1);
+    superviseur.rames.push_back(rame2);
+
+    superviseur.recupererPositionsRames();
+
+    return 0;
 }
--- Tram-project/Tram-project.h
+++ Tram-project/Tram-project.h
@@ -4,5 +4,58 @@
 #pragma once
 
 #include <iostream>
+#include <iostream>
+#include <vector>
 
-// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
+class Rame {
+public:
+    int numero;
+    double position;
+    double vitesse;
+    int nombrePassagers;
+
+    // Autres attributs et méthodes nécessaires pour la gestion des rames
+
+    Rame(int num, double pos, double vit, int passagers)
+        : numero(num), position(pos), vitesse(vit), nombrePassagers(passagers) {}
+
+    // Méthode pour déplacer la rame
+    void deplacer() {
+        // Logique pour déplacement de la rame
+    }
+
+    // Autres méthodes de la rame
+};
+
+class Station {
+public:
+    int nombrePassagersQuai;
+    bool ramePresente;
+
+    // Autres attributs et méthodes nécessaires pour la gestion des stations
+
+    Station() : nombrePassagersQuai(0), ramePresente(false) {}
+
+    // Méthode pour signaler à la rame qu'elle peut quitter la station
+    void signalerDepartRame() {
+        // Logique pour signaler le départ
+    }
+
+    // Autres méthodes de la station
+};
+
+class Superviseur {
+public:
+    std::vector<Rame> rames;
+
+    // Autres attributs et méthodes nécessaires pour la gestion du superviseur
+
+    // Méthode pour récupérer la position de toutes les rames
+    void recupererPositionsRames() {
+        for (const auto& rame : rames) {
+            std::cout << "Rame " << rame.numero << " - Position : " << rame.position << std::endl;
+        }
+    }
+
+    // Autres méthodes du superviseur
+};
