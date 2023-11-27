ce1207be8e07a4bcf17b6eb1dd75af414bf9afa5
baptiste1908 <baptiste.gilleron@student.junia.com>
2023-11-27 08:20:09 +0100

    New modif aavant changement

--- Tram-project/Tram-project.cpp
+++ Tram-project/Tram-project.cpp
@@ -1,24 +1,200 @@
-﻿// Tram-project.cpp : définit le point d'entrée de l'application.
-//
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <SFML/Graphics.hpp>
 
-#include "Tram-project.h"
+/*class Rame;
+class Station;
 
-using namespace std;
+class Superviseur {
+private:
+    std::vector<Rame*> rames;
 
+public:
+    void ajouterRame(Rame* rame) {
+        rames.push_back(rame);
+    }
 
+    void arretUrgenceAleatoire() {
+        if (!rames.empty()) {
+            int indiceRameArretUrgence = rand() % rames.size();
+            Rame* rameArretUrgence = rames[indiceRameArretUrgence];
+            std::cout << "Arrêt d'urgence pour la rame " << rameArretUrgence->getNumero() << std::endl;
+            rameArretUrgence->arretUrgence();
+        }
+    }
+
+    void getPositionRames() const {
+        std::cout << "Position des rames : " << std::endl;
+        for (const auto& rame : rames) {
+            std::cout << "Rame " << rame->getNumero() << " - Position : " << rame->getPosition() << std::endl;
+        }
+    }
+};
+
+class Rame {
+private:
+    int numero;
+    double position;
+    double vitesse;
+    const double vitesseCroisiere = 30.0;
+    int nombrePassagers;
+
+public:
+    Rame(int numero) : numero(numero), position(0.0), vitesse(0.0), nombrePassagers(0) {}
+
+    void quitterStation() {
+        accelerer(5.0);
+        std::cout << "La rame " << numero << " quitte la station. Nouvelle vitesse : " << vitesse << std::endl;
+    }
+
+    void parcourirLigne() {
+        maintenirVitesseCroisiere();
+        std::cout << "La rame " << numero << " parcourt la ligne à vitesse de croisière : " << vitesse << std::endl;
+    }
+
+    void arreterAStation() {
+        decelerer(5.0);
+        std::cout << "La rame " << numero << " ralentit en approchant de la station : " << vitesse << std::endl;
+        vitesse = 0.0;
+        std::cout << "La rame " << numero << " est à l'arrêt à la station." << std::endl;
+    }
+
+    void gererPassagers(Station& station) {
+        station.rameArrive();
+
+        int passagersEntrent = rand() % 21;
+        int passagersSortent = rand() % (nombrePassagers + 1);
+
+        if (passagersSortent > nombrePassagers) {
+            passagersSortent = nombrePassagers;
+        }
+
+        nombrePassagers -= passagersSortent;
+
+        int capaciteMax = 100;
+        if (nombrePassagers + passagersEntrent <= capaciteMax) {
+            nombrePassagers += passagersEntrent;
+        }
+        else {
+            std::cout << "La rame est pleine. Certains passagers restent sur le quai." << std::endl;
+        }
+
+        std::cout << "La rame " << numero << " - Nombre de passagers à bord : " << nombrePassagers << std::endl;
+    }
+
+    void arretUrgence() {
+        vitesse = 0.0;
+        std::cout << "Arrêt d'urgence effectué pour la rame " << numero << "." << std::endl;
+    }
+
+    double getPosition() const {
+        return position;
+    }
+
+    int getNumero() const {
+        return numero;
+    }
+
+private:
+    void accelerer(double acceleration) {
+        vitesse += acceleration;
+    }
+
+    void decelerer(double deceleration) {
+        vitesse -= deceleration;
+    }
+
+    void maintenirVitesseCroisiere() {
+        vitesse = vitesseCroisiere;
+    }
+};
+
+class Station {
+private:
+    int nombrePassagersQuai;
+
+public:
+    Station() : nombrePassagersQuai(0) {}
+
+    void rameArrive() {
+        int passagersEntrent = rand() % 21;
+        int passagersSortent = rand() % (nombrePassagersQuai + 1);
+
+        if (passagersSortent > nombrePassagersQuai) {
+            passagersSortent = nombrePassagersQuai;
+        }
+
+        nombrePassagersQuai -= passagersSortent;
+        nombrePassagersQuai += passagersEntrent;
+
+        std::cout << "Nombre de passagers sur le quai : " << nombrePassagersQuai << std::endl;
+    }
+
+    int getNombrePassagersQuai() const {
+        return nombrePassagersQuai;
+    }
+
+    bool autoriserDepartRame() const {
+        return nombrePassagersQuai < 10;
+    }
+};
+*/
 int main() {
-    // Exemple d'utilisation des classes
-    Rame rame1(1, 0.0, 30.0, 50);
-    Rame rame2(2, 10.0, 25.0, 40);
+   /* srand(static_cast<unsigned int>(time(0)));
 
-    Station station1;
-    station1.nombrePassagersQuai = 15;
+    Rame rame1(1);
+    Rame rame2(2);
+
+    Station station;
 
     Superviseur superviseur;
-    superviseur.rames.push_back(rame1);
-    superviseur.rames.push_back(rame2);
+    superviseur.ajouterRame(&rame1);
+    superviseur.ajouterRame(&rame2);
 
-    superviseur.recupererPositionsRames();
+    for (int i = 0; i < 5; ++i) {
+        superviseur.getPositionRames();
 
-    return 0;
+        rame1.quitterStation();
+        rame1.parcourirLigne();
+        rame1.gererPassagers(station);
+        rame1.arreterAStation();
+        rame1.gererPassagers(station);
+
+        rame2.quitterStation();
+        rame2.parcourirLigne();
+        rame2.gererPassagers(station);
+        rame2.arreterAStation();
+        rame2.gererPassagers(station);
+
+        if (rand() % 10 == 0) {
+            superviseur.arretUrgenceAleatoire();
+        }
+    }
+
+    return 0;*/
+
+
+
+        sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+                window.draw(shape);
+                window.display();
+        }
+
+        return 0;
+    
 }
--- Tram-project/Tram-project.h
+++ Tram-project/Tram-project.h
@@ -1,61 +1,117 @@
-﻿// Tram-project.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets.
-
-#pragma once
-
-#include <iostream>
-#include <iostream>
-#include <vector>
-
-class Rame {
-public:
-    int numero;
-    double position;
-    double vitesse;
-    int nombrePassagers;
-
-    // Autres attributs et méthodes nécessaires pour la gestion des rames
-
-    Rame(int num, double pos, double vit, int passagers)
-        : numero(num), position(pos), vitesse(vit), nombrePassagers(passagers) {}
-
-    // Méthode pour déplacer la rame
-    void deplacer() {
-        // Logique pour déplacement de la rame
-    }
-
-    // Autres méthodes de la rame
-};
-
-class Station {
-public:
-    int nombrePassagersQuai;
-    bool ramePresente;
-
-    // Autres attributs et méthodes nécessaires pour la gestion des stations
-
-    Station() : nombrePassagersQuai(0), ramePresente(false) {}
-
-    // Méthode pour signaler à la rame qu'elle peut quitter la station
-    void signalerDepartRame() {
-        // Logique pour signaler le départ
-    }
-
-    // Autres méthodes de la station
-};
-
-class Superviseur {
-public:
-    std::vector<Rame> rames;
-
-    // Autres attributs et méthodes nécessaires pour la gestion du superviseur
-
-    // Méthode pour récupérer la position de toutes les rames
-    void recupererPositionsRames() {
-        for (const auto& rame : rames) {
-            std::cout << "Rame " << rame.numero << " - Position : " << rame.position << std::endl;
-        }
-    }
-
-    // Autres méthodes du superviseur
-};
+﻿//// Tram-project.h : fichier Include pour les fichiers Include système standard,
+//// ou les fichiers Include spécifiques aux projets.
+//
+//#pragma once
+//
+//#include <iostream>
+//#include <vector>
+//#include <thread>
+//#include <mutex>
+//#include <chrono>
+//std::mutex mtx; // Mutex pour la synchronisation
+//
+//class Rame {
+//public:
+//    int numero;
+//    double position;
+//    double vitesse;
+//    int nombrePassagers;
+//    bool arretUrgence;
+//
+//    Rame(int num, double pos, double vit, int passagers)
+//        : numero(num), position(pos), vitesse(vit), nombrePassagers(passagers), arretUrgence(false) {}
+//
+//    void deplacer() {
+//        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Simulation du déplacement
+//        position += vitesse;
+//    }
+//
+//    void gestionRame() {
+//        while (true) {
+//            std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Simulation de la gestion de la rame
+//            deplacer();
+//
+//            // Logique pour la gestion des passagers, accélération, décélération, etc.
+//            // ...
+//
+//            // Utilisez le mutex pour synchroniser l'accès aux données partagées
+//            std::lock_guard<std::mutex> lock(mtx);
+//            if (arretUrgence) {
+//                std::cout << "Rame " << numero << " - Arrêt d'urgence !" << std::endl;
+//                arretUrgence = false; // Réinitialiser l'arrêt d'urgence
+//            }
+//            else {
+//                std::cout << "Rame " << numero << " - Position : " << position << std::endl;
+//            }
+//        }
+//    }
+//};
+//
+//class Station {
+//public:
+//    int nombrePassagersQuai;
+//    bool ramePresente;
+//
+//    Station() : nombrePassagersQuai(0), ramePresente(false) {}
+//
+//    void signalerDepartRame() {
+//        std::this_thread::sleep_for(std::chrono::milliseconds(200)); // Simulation du signal de départ
+//        ramePresente = false;
+//    }
+//
+//    void gestionStation(Rame& rame) {
+//        while (true) {
+//            std::this_thread::sleep_for(std::chrono::milliseconds(1500)); // Simulation de la gestion de la station
+//            // Logique pour la gestion des passagers, signalisation à la rame, etc.
+//            // ...
+//
+//            // Utilisez le mutex pour synchroniser l'accès aux données partagées
+//            std::lock_guard<std::mutex> lock(mtx);
+//            if (ramePresente) {
+//                if (rame.nombrePassagers < 50) {
+//                    int passagersEntrants = rand() % (50 - rame.nombrePassagers + 1);
+//                    rame.nombrePassagers += passagersEntrants;
+//                    nombrePassagersQuai -= passagersEntrants;
+//                    std::cout << passagersEntrants << " passagers entrent dans la rame." << std::endl;
+//                }
+//                if (nombrePassagersQuai > 0) {
+//                    int passagersSortants = rand() % (rame.nombrePassagers + 1);
+//                    rame.nombrePassagers -= passagersSortants;
+//                    nombrePassagersQuai += passagersSortants;
+//                    std::cout << passagersSortants << " passagers sortent de la rame." << std::endl;
+//                }
+//            }
+//            else {
+//                std::cout << "Pas de rame à la station." << std::endl;
+//            }
+//        }
+//    }
+//};
+//
+//class Superviseur {
+//public:
+//    std::vector<Rame> rames;
+//
+//    void recupererPositionsRames() {
+//        while (true) {
+//            std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // Simulation de la récupération des positions
+//            // Logique pour récupérer les positions des rames
+//            // ...
+//
+//            // Utilisez le mutex pour synchroniser l'accès aux données partagées
+//            std::lock_guard<std::mutex> lock(mtx);
+//            for (const auto& rame : rames) {
+//                std::cout << "Rame " << rame.numero << " - Position : " << rame.position << std::endl;
+//            }
+//        }
+//    }
+//};            std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // Simulation de la récupération des positions
+//            // Ajoutez la logique pour récupérer les positions des rames
+//            // Utilisez le mutex pour synchroniser l'accès aux données partagées
+//            std::lock_guard<std::mutex> lock(mtx);
+//            for (const auto& rame : rames) {
+//                std::cout << "Rame " << rame.numero << " - Position : " << rame.position << std::endl;
+//            }
+//        }
+//    }
+//};
